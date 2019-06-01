49dcbc574026ab02dc2fe401f77c8ab3f3749fad
Queenvictoria <snow@hollysydney.com>
2019-06-01 11:12:10 +1000

    Functioning with a rate limiter.

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ require('dotenv').config()
 // godaddy = GoDaddy(process.env.API_KEY, process.env.API_SECRET);
 
 const gd_domains = require('./node_modules/godaddy-api/lib/domains').domains;
+const Bottleneck = require('bottleneck');
+
+const limiter = new Bottleneck({
+  maxConcurrent: 1,
+  minTime: 1000
+});
 
 const data = require('./data/domains.json');
 
@@ -28,43 +34,52 @@ const domainOrOptions = {
 domainsAPI =  new gd_domains(domainOrOptions);
 
 
-// const domain = 'agentallblack.com'
-// updateNS(domain, data[domain]);
-
 Object.keys(data).forEach(domain => {
   updateNS(domain, data[domain]);
 });
 
 
+/*
+ * Accepts a domain and a data object.
+ * The data object should contain a property 'NS' which is an array of nameservers.
+ */
 function updateNS(domain, data) {
-  console.log(`Updating ${domain}.`);
-// Get a domain
-
-  domainsAPI.get({domain: domain})
+  // Get a domain
+  // domainsAPI.get({domain: domain})
+  limiter.schedule(() => domainsAPI.get({domain: domain})
     .then(res => {
       // Examine it's NS record
       // If our nameServers are houseoflaudanum then update to our new ones.
-      if ( res.body && res.body.nameServers.pop().toLowerCase().indexOf('houseoflaudanum') > 0 ) {
+      if ( res.body && res.body.nameServers && res.body.nameServers.pop().toLowerCase().indexOf('houseoflaudanum') > 0 ) {
         const params = {domain: domain, body:{nameServers: data.NS}};
         // Update it's NS record
         return domainsAPI.update(params);
       }
+      else if ( res.body && res.body.nameServers && res.body.nameServers.pop().toLowerCase().indexOf('googledomains') > 0 ) {
+        console.error(`${domain} already updated. SKIPPING.`);
+        return res;
+      }
       else {
-        console.error(`${domain} did not use houseoflaudanum NS. SKIPPING.`);
+        if ( res.body && res.body.status)
+          console.error(`${domain} did not use houseoflaudanum NS (${res.body.status}). SKIPPING.`);
+        else
+          console.error(`${domain} did not use houseoflaudanum NS. SKIPPING.`);
         return res;
       }
     })
-    .then(res => {
-      if ( res && res.body && res.body.nameServers)
-        console.log(res.body.nameServers);
-      else
-        console.log(res.body);
-    })
     .catch(err => {
-      console.error("err");
-      if ( err.body )
-        console.error(err.body);
-      else
+      if ( err.body ) {
+        if ( err.body.message && err.body.message.indexOf('not found for shopper') )
+          console.error(`${domain} not in this account. SKIPPING.`)
+        else {
+          console.error(`Error for ${domain}.`);
+          console.error(err.body);
+        }
+      }
+      else {
+        console.error(`Error for ${domain}.`);
         console.error(err);
+      }
     })
+  );
 }
